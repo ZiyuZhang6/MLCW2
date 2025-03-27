64e3510dcc2348811a89c0eeb08bf90090df9cde
ZiyuZhang6 <K23069561@kcl.ac.uk>
2025-03-27 14:40:56 +0000

    Update tpc_sample_selection.py

--- tpc_sample_selection.py
+++ tpc_sample_selection.py
@@ -28,7 +28,7 @@ if torch.cuda.is_available():
     torch.cuda.manual_seed_all(seed_value)
 
 # Define device to use GPU if available otherwise "cpu"
-device = torch.device("cuda")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Data preprocessing
 strong_transform = transforms.Compose([
